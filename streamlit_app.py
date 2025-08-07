f5a16e656300457bee3e14b9127b13206c3a8881
malekbenhafed <serinebenhafed8@gmail.com>
2025-08-07 18:14:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('Earthquake detector')
 
 st.write('The goal is to detect possible earthquake risks before something goes wrong')
 
-df = pd.read_csc('Steel_Plates_Faults.csv')
+df = pd.read_csc('https://github.com/malekbenhafed/final-project/blob/master/Steel_Plates_Faults.csv')
 df
