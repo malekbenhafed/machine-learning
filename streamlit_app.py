4e40163a073ab642d381aadb0fdd2ced312ef91b
malekbenhafed <serinebenhafed8@gmail.com>
2025-08-07 17:53:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Earthquake detector')
 
 st.write('The goal is to detect possible earthquake risks before something goes wrong')
+
+df = pd.read_csc('Steel_Plates_Faults.csv')
+df
