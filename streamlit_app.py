8cd49664f628578bd6453b2d7fec9c0d76525b4f
malekbenhafed <serinebenhafed8@gmail.com>
2025-08-08 22:39:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.title('🌍 Earthquake Risk Detector')
 st.write("This app predicts steel plate fault types to help assess potential earthquake vulnerabilities.")
- 
+
+with st.expander('Data'):
+st.write('**Raw data**')
 df = pd.read_csv('https://raw.githubusercontent.com/malekbenhafed/final-project/master/Steel_Plates_Faults.csv')
 st.dataframe(df)
