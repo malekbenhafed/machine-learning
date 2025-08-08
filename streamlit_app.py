817f1db86bc25df2c1c6de678b58addb9b436d4f
malekbenhafed <serinebenhafed8@gmail.com>
2025-08-08 22:45:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,10 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/malekbenhafed/final-project/master/Steel_Plates_Faults.csv')
   st.dataframe(df)
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('**y**')
+  y= df.species
+  y
