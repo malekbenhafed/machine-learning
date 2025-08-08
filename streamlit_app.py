11ce3912b2ece6156f2db30f0b8265a57e199688
malekbenhafed <serinebenhafed8@gmail.com>
2025-08-08 22:48:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,12 @@ with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/malekbenhafed/final-project/master/Steel_Plates_Faults.csv')
   st.dataframe(df)
   st.write('**X**')
+  df = pd.read_csv('Steel_Plates_Faults.csv')
+
+  st.write('**X (Features)**')
   X_raw = df.drop(['Pastry', 'Z_Scratch', 'K_Scratch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults'], axis=1)
   st.dataframe(X_raw)
 
- st.write('**y**')
- y_raw = df[['Pastry', 'Z_Scratch', 'K_Scratch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults']]
- st.dataframe(y_raw)
+  st.write('**y (Target - Fault Types)**')
+  y_raw = df[['Pastry', 'Z_Scratch', 'K_Scratch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults']]
+  st.dataframe(y_raw)
